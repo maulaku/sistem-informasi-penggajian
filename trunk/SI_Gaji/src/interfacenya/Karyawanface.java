b9bada84ececc769e5971b2334162b92820cc6b6
tdvnabul@gmail.com <tdvnabul@gmail.com@d4b48217-3e33-4e06-7e49-61e9d791654c>
2012-07-18 05:19:21 +0000

    Interface karyawan

---
+++ trunk/SI_Gaji/src/interfacenya/Karyawanface.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacenya;
+
+import entity.tbKaryawan;
+import java.util.List;
+
+/**
+ *
+ * @author Hotasta
+ */
+public interface Karyawanface extends Generalface {
+    public String update(long id, tbKaryawan k);
+    public String delete(long  id);
+    
+    public tbKaryawan get (long id);
+    public List<tbKaryawan> getALL();
+}
