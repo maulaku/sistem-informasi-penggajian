c23ac1bcb9bda35a762f5d3e1cbbcfe025d0b6c9
dhewaardanz@gmail.com <dhewaardanz@gmail.com@d4b48217-3e33-4e06-7e49-61e9d791654c>
2012-07-18 11:39:13 +0000

    interface golongan

---
+++ SI_Gaji/src/interfacenya/Golonganface.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacenya;
+
+import entity.tbGolongan;
+import java.util.List;
+
+/**
+ *
+ * @author Dhurand
+ */
+public interface Golonganface extends Generalface {
+    public String update(long id,tbGolongan g);
+    public String delete(long id);
+    public tbGolongan get(long id);
+    public List<tbGolongan> getAll();
+    
+    
+}
