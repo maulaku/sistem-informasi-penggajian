d96cb03170907d2e39d26dd100cbaa0621fa1875
dhewaardanz@gmail.com <dhewaardanz@gmail.com@d4b48217-3e33-4e06-7e49-61e9d791654c>
2012-07-18 02:19:06 +0000

    coba commit

---
+++ trunk/SI_Gaji/src/entity/tbGolongan.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Dhurand
+ */
+@Entity
+public class tbGolongan implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(nullable=false,unique=true)
+    private String golongan;
+    @Column(nullable=false)
+    private double gajiPokok;
+    @Column(nullable=false)
+    private double pajak;
+
+    public double getGajiPokok() {
+        return gajiPokok;
+    }
+
+    public void setGajiPokok(double gajiPokok) {
+        this.gajiPokok = gajiPokok;
+    }
+
+    public String getGolongan() {
+        return golongan;
+    }
+
+    public void setGolongan(String golongan) {
+        this.golongan = golongan;
+    }
+
+    public double getPajak() {
+        return pajak;
+    }
+
+    public void setPajak(double pajak) {
+        this.pajak = pajak;
+    }
+    
+    
+    
+}
