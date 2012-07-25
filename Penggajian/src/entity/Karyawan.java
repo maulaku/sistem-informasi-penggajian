891248ab2f34ccc34c86cb98fa83cb072477ec54
fauzia.ps@gmail.com <fauzia.ps@gmail.com@d4b48217-3e33-4e06-7e49-61e9d791654c>
2012-07-25 05:17:19 +0000

    entity karyawan

---
+++ Penggajian/src/entity/Karyawan.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Fauzia
+ */
+@Entity
+public class Karyawan implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(length=10,nullable=false,unique=true)
+    private String NIK;
+    @Column(nullable=false)
+    private String nama;
+    @Column(nullable=false)
+    private String golongan;
+    @Column(nullable=false)
+    private String gender;
+    @Column(nullable=false)
+    private String alamat;
+
+    public String getNIK() {
+        return NIK;
+    }
+
+    public void setNIK(String NIK) {
+        this.NIK = NIK;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getGolongan() {
+        return golongan;
+    }
+
+    public void setGolongan(String golongan) {
+        this.golongan = golongan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    
+    
+}
