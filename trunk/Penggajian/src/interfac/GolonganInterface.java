d3e49c220f321e54bfd9cf2d87a6f7db638af8bc
adi.672009052@gmail.com <adi.672009052@gmail.com@d4b48217-3e33-4e06-7e49-61e9d791654c>
2012-07-25 05:28:18 +0000

    interface golongan

---
+++ trunk/Penggajian/src/interfac/GolonganInterface.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfac;
+
+import entity.Golongan;
+import java.util.List;
+
+/**
+ *
+ * @author Adi
+ */
+public interface GolonganInterface {
+    
+    public String Insert(Golongan g);
+    public String Update(String kode,Golongan g);
+    public String Delete(String kode);
+    public List<Golongan> getByKode(String kode);
+    public List<Golongan> getAll();
+}
